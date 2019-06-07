b958b33786c6798dbd0b6c22fa3018f53f95f887
NK <nk@nk.ru>
2019-06-07 18:23:55 +0700

    Added dateExtensions

---
+++ TestAutoCatalog/Utilities/Extensions/DateExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  DateExtensions.swift
+//  TestAutoCatalog
+//
+//  Created by nic on 07/06/2019.
+//  Copyright © 2019 nic. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    func getYearAsString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
