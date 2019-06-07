e4f174b2af2bf94daa1002931b0fbc9035f033f0
NK <nk@nk.ru>
2019-06-07 18:24:28 +0700

    Added Box.swift for dynamic binding

---
+++ TestAutoCatalog/Utilities/Box.swift
@@ -0,0 +1,28 @@
+//
+//  Box.swift
+//  TestAutoCatalog
+//
+//  Created by nic on 07/06/2019.
+//  Copyright © 2019 nic. All rights reserved.
+//
+
+import Foundation
+
+class Box<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
