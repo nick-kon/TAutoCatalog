48346081217f07bc8c08feb0bb19ee67b75b133c
NK <nk@nk.ru>
2019-06-08 21:23:52 +0700

    Modified enums in carModel . Adding protocol: StoredAsEnum

--- TestAutoCatalog/Model/CarModel.swift
+++ TestAutoCatalog/Model/CarModel.swift
@@ -10,7 +10,8 @@ import Foundation
 
 /// Classes of car in Europe
 
-enum CarClass: String, Codable {
+enum CarClass: String, Codable, StoredAsEnum, CaseIterable {
+    
     case a = "mini"   // Mini cars
     case b = "small"   // small cars
     case c = "medium"   // medium cars
@@ -20,9 +21,35 @@ enum CarClass: String, Codable {
     case j = "suv"   // sport utility (suv)
     case m = "multipurpose"   // multipurpose
     case s = "sportCoupe"   // sport coupe
+    
+    func toString() -> String {
+        switch self {
+        case .a:
+            return Constants.UI.CarClasses.a
+        case .b:
+            return Constants.UI.CarClasses.b
+        case .c:
+            return Constants.UI.CarClasses.c
+        case .d:
+            return Constants.UI.CarClasses.d
+        case .e:
+            return Constants.UI.CarClasses.e
+        case .f:
+            return Constants.UI.CarClasses.f
+        case .j:
+            return Constants.UI.CarClasses.j
+        case .m:
+            return Constants.UI.CarClasses.m
+        case .s:
+            return Constants.UI.CarClasses.s
+        }
+        
+    }
+
 }
 
-enum CarBodyStyle: String, Codable {
+enum CarBodyStyle: String, Codable, StoredAsEnum, CaseIterable {
+
     case suv = "suv"
     case truck = "truck"
     case sedan = "sedan"
@@ -35,6 +62,36 @@ enum CarBodyStyle: String, Codable {
     case crossover = "crossover"
     case luxury = "luxury"
     case hybrid = "hybrid"
+    
+    func toString() -> String {
+        switch self {
+        case .convertible:
+            return Constants.UI.CarBodyStyle.convertible
+        case .coupe:
+            return Constants.UI.CarBodyStyle.coupe
+        case .crossover:
+            return Constants.UI.CarBodyStyle.crossover
+        case .diesel:
+            return Constants.UI.CarBodyStyle.diesel
+        case .hybrid:
+            return Constants.UI.CarBodyStyle.hybrid
+        case .luxury:
+            return Constants.UI.CarBodyStyle.luxury
+        case .sedan:
+            return Constants.UI.CarBodyStyle.sedan
+        case .sport:
+            return Constants.UI.CarBodyStyle.sport
+        case .suv:
+            return Constants.UI.CarBodyStyle.suv
+        case .truck:
+            return Constants.UI.CarBodyStyle.truck
+        case .van:
+            return Constants.UI.CarBodyStyle.van
+        case .wagon:
+            return Constants.UI.CarBodyStyle.wagon
+        }
+    }
+    
 }
 
 struct CarModel: Codable {
---
+++ TestAutoCatalog/Utilities/Protocols/StoredAsEnum.swift
@@ -0,0 +1,13 @@
+//
+//  StoredAsEnum.swift
+//  TestAutoCatalog
+//
+//  Created by nic on 08/06/2019.
+//  Copyright © 2019 nic. All rights reserved.
+//
+
+import Foundation
+
+protocol StoredAsEnum {
+    func toString() -> String
+}
